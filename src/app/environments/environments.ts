582d6d2bc972c98f96612ad81cc7eaf7b4cca841
erekle ratiani <62502052+eratiani@users.noreply.github.com>
2023-03-27 14:55:00 +0400

    feat: change deploy url
    railway data used up changed deploy url to localhost

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: false,
-  // apiUrl: 'http://localhost:3000',
-  apiUrl: 'https://final-task-backend-production-d9d3.up.railway.app',
+  apiUrl: 'http://localhost:3000',
+  /// railway data limit used up if its start of month try code below.
+  // apiUrl: 'https://final-task-backend-production-d9d3.up.railway.app',
+  //app with backend deployed at.
+  // https://project-management-app-deploy.web.app/project-management-app/Home
 };
